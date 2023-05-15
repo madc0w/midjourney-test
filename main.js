dd2fc6b8169d4746a13d5788efd7862eafc2e3e4
Marc A. Donis <marc@balloonary.io>
2023-05-15 12:09:24 +0200

    .

--- main.js
+++ main.js
@@ -9,180 +9,82 @@ const sharp = require('sharp');
 const baseUrl = 'https://api.midjourneyapi.io';
 const apiKey = process.env.MIDJOURNEY_API_KEY;
 
-const prompt = 'a happy girl holding purple balloons on fire, 8k, --ar 3:2';
+let prompt = 'happy girl holding cute guinea pig --ar 3:2';
 
 (async () => {
 	// https://docs.midjourneyapi.io/midjourney-api/midjourney-api/imagine
 
-	let resultId;
-	// {
-	// 	const config = baseConfig(
-	// 		{
-	// 			// callbackURL: 'https://....', // Optional
-	// 			prompt,
-	// 		},
-	// 		'imagine'
-	// 	);
-	// 	const response = await axios.request(config);
-	// 	// log('imagine data', JSON.stringify(response.data));
-	// 	log('credits', JSON.stringify(response.headers['midapi-credits']));
+	for (let i = 0; i < 8; i++) {
+		log(`prompt ${i}:`, prompt);
+		let resultId;
+		{
+			const config = baseConfig(
+				{
+					// callbackURL: 'https://....', // Optional
+					prompt,
+				},
+				'imagine'
+			);
+			const response = await axios.request(config);
+			// log('imagine data', JSON.stringify(response.data));
+			// log('credits', JSON.stringify(response.headers['midapi-credits']));
 
-	// 	resultId = response.data.resultId;
-	// }
-	// log('resultId', resultId);
+			resultId = response.data.resultId;
+		}
+		// log('resultId', resultId);
 
-	// const imageUrl = await getResult(resultId, 'imageURL');
-	// log('imageUrl', imageUrl);
-	// const filename = imageUrl.substring(imageUrl.lastIndexOf('/') + 1);
-	// await downloadAndSaveImage(imageUrl, `out/${filename}`);
-	// log(`wrote image out/${filename}`);
+		const imagineResult = await getResult(resultId);
+		const imageUrl = imagineResult.imageURL;
+		// log('imagineResult', imagineResult);
+		const filename = imageUrl.substring(imageUrl.lastIndexOf('/') + 1);
+		await downloadAndSaveImage(imageUrl, `out/${filename}`);
+		// log(`wrote image out/${filename}`);
 
-	const filename =
-		'badygobylamy_a_happy_girl_holding_purple_balloons_on_fire_8k_7fa7b69b-a433-4726-9666-a8add08433ae.png';
+		{
+			const config = baseConfig(
+				{
+					messageId: imagineResult.messageId,
+					jobId: imagineResult.jobId,
+					position: 1,
+				},
+				'upscale'
+			);
+			const response = await axios.request(config);
+			// log('upscale', JSON.stringify(response.data));
+			resultId = response.data.resultId;
+		}
+		const upscaledImageUrl = (await getResult(resultId)).imageURL;
+		const upscaledFilename =
+			filename.substring(0, filename.lastIndexOf('.')) + '-upscale.png';
+		await downloadAndSaveImage(upscaledImageUrl, `out/${upscaledFilename}`);
 
-	{
-		const outFilename =
-			filename.substring(0, filename.lastIndexOf('.')) + '-small.png';
-		await scaleImage(`out/${filename}`, `out/${outFilename}`, 100, 100);
-		const data = new FormData();
-		data.append('image', fs.createReadStream(`out/${outFilename}`), filename);
-		// data.append('callbackURL', 'https://....'); // Optional
+		{
+			const smallFilename =
+				filename.substring(0, filename.lastIndexOf('.')) + '-small.png';
+			await scaleImage(
+				`out/${upscaledFilename}`,
+				`out/${smallFilename}`,
+				400,
+				400
+			);
+			const data = new FormData();
+			data.append(
+				'image',
+				fs.createReadStream(`out/${smallFilename}`),
+				filename
+			);
+			// data.append('callbackURL', 'https://....'); // Optional
 
-		const config = {
-			method: 'post',
-			maxBodyLength: Infinity,
-			url: `${baseUrl}/describe`,
-			headers: {
-				Authorization: apiKey,
-				...data.getHeaders(),
-			},
-			data,
-		};
+			const config = baseConfig(null, 'describe', data.getHeaders());
+			config.data = data;
+			const response = await axios.request(config);
 
-		const response = await axios.request(config);
-
-		// const config = baseConfig(formData.getHeaders(), 'describe', formData);
-		// const response = await axios.request(config);
-
-		// const response = await axios.post(`${baseUrl}/describe`, formData, {
-		// 	maxBodyLength: Infinity,
-		// 	data: JSON.stringify({
-		// 		image: fs.readFileSync(`out/${outFilename}`),
-		// 	}),
-		// 	headers: {
-		// 		Authorization: apiKey,
-		// 		...formData.getHeaders(),
-		// 	},
-		// });
-
-		log('describe', JSON.stringify(response.data));
-		resultId = response.data.resultId;
+			// log('describe', JSON.stringify(response.data));
+			resultId = response.data.resultId;
+		}
+		const description = (await getResult(resultId)).content;
+		prompt = description[0].substring(4);
 	}
-	const description = await getResult(resultId, 'content');
-	log('description', description);
-
-	// /**********  IMAGINE  ***********/
-
-	// // Imagine request
-	// const req1 = await midjourney.imagine('a red knight riding a blue horse');
-
-	// // await sleep(30 * 1000);
-
-	// // Get result
-	// let status, res1;
-	// do {
-	// 	res1 = await midjourney.getResult(req1.resultId);
-	// 	log('res1', res1);
-	// 	status = res1.status;
-	// } while (status == 'pending');
-
-	// /**********  IMAGINE  ***********/
-
-	// /**********  UPSCALE IMAGE #2  ***********/
-
-	// // Upscale request
-	// const req2 = await midjourney.upscale(res1.messageId, res1.jobId, 2);
-
-	// await sleep(30 * 1000);
-
-	// // Get result
-	// const res2 = await midjourney.getResult(req2.resultId);
-	// log('res2', res2);
-	// process.exit();
-
-	// /**********  UPSCALE IMAGE #2 ***********/
-
-	// /**********  VARIATIONS OF IMAGE #3 ***********/
-
-	// // Variations request
-	// const req3 = await midjourney.variations(res1.messageId, res1.jobId, 3);
-
-	// await sleep(30 * 1000);
-
-	// // Get result
-	// const res3 = await midjourney.getResult(req3.resultId);
-
-	// /**********  VARIATIONS OF IMAGE #3  ***********/
-
-	// /**********  GET SEED OF THE FIRST COMMAND  ***********/
-
-	// // seed request
-	// const req4 = await midjourney.getSeed(res1.messageId, res1.jobId);
-
-	// await sleep(5 * 1000);
-
-	// // Get result
-	// const res4 = await midjourney.getResult(req4.resultId);
-	// log('res4 ', res4);
-
-	// /**********  GET SEED OF THE FIRST COMMAND ***********/
-
-	// /**********  BLEND TWO IMAGES  ***********/
-
-	// // seed request
-	// const req5 = await midjourney.blend(
-	// 	[
-	// 		{
-	// 			// FILE 1
-	// 			file: fs.createReadStream('./images/image1.png'),
-	// 			name: 'image1.png',
-	// 		},
-	// 		{
-	// 			// FILE 2
-	// 			file: fs.createReadStream('./images/image2.png'),
-	// 			name: 'image2.png',
-	// 		},
-	// 		{
-	// 			// FILE 3
-	// 			file: fs.createReadStream('./images/image3.png'),
-	// 			name: 'image3.png',
-	// 		},
-	// 	],
-	// 	'Landscape'
-	// );
-
-	// await sleep(30 * 1000);
-
-	// // Get result
-	// const res5 = await midjourney.getResult(req5.resultId);
-
-	// /**********  BLEND TWO IMAGES ***********/
-
-	// /**********  DESCRIBE AN IMAGE  ***********/
-
-	// // seed request
-	// const req6 = await midjourney.describe({
-	// 	// FILE 1
-	// 	file: fs.createReadStream('./images/image2.png'),
-	// 	name: 'image2.png',
-	// });
-
-	// await sleep(30 * 1000);
-
-	// // Get result
-	// const res6 = await midjourney.getResult(req6.resultId);
-
-	// /**********  DESCRIBE AN IMAGE  ***********/
 })();
 
 async function downloadAndSaveImage(url, imagePath) {
@@ -191,6 +93,7 @@ async function downloadAndSaveImage(url, imagePath) {
 }
 
 function baseConfig(data, endpoint, headers = {}) {
+	// log('baseConfig endpoint', endpoint);
 	return {
 		method: 'post',
 		maxBodyLength: Infinity,
@@ -204,8 +107,8 @@ function baseConfig(data, endpoint, headers = {}) {
 	};
 }
 
-async function getResult(resultId, key) {
-	let isWaiting, result, credits;
+async function getResult(resultId) {
+	let isWaiting, result, credits, prevStatus, prevProgress;
 	do {
 		const config = baseConfig(
 			{
@@ -217,17 +120,24 @@ async function getResult(resultId, key) {
 		const response = await axios.request(config);
 		// log('response', response.data);
 		if (response.data.status) {
-			log('status:', response.data.status);
-			if (response.data.percentage) {
+			if (prevStatus != response.data.status) {
+				prevStatus = response.data.status;
+				log('status:', response.data.status);
+			}
+			if (
+				response.data.percentage &&
+				prevProgress != response.data.percentage
+			) {
+				prevProgress = response.data.percentage;
 				log(response.data.percentage + '%');
 			}
 		} else if (!isWaiting) {
 			log('waiting...');
 			isWaiting = true;
 		}
-		result = response.data[key];
+		result = response.data;
 		credits = response.headers['midapi-credits'];
-	} while (!result);
+	} while (prevStatus != 'completed');
 	log('remaining credits:', credits);
 	return result;
 }
